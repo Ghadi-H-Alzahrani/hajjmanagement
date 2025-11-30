4b402b250bae3c92d908ec5b53b447ea091ef8a9
ghadi-h-alzahrani <ghadihz2003@gmail.com>
2025-11-30 10:48:45 +0300

    internal server error

--- config/database.php
+++ config/database.php
@@ -60,6 +60,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // If you also want to set mysql ssl mode directly (Laravel 8+ supports 'sslmode'):
+                // 'ssl_mode' => env('DB_SSL_MODE', 'REQUIRED'), // optional
             ]) : [],
         ],
 
@@ -148,7 +150,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
